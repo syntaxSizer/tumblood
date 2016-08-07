2be7846b7960d39ae17b786ce72998541110ef98
syntaxSizer <kakuma4never@gmail.com>
2016-08-07 03:32:05 +0300

    add app models

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
+from flask.ext.mongegine import MongoEngine
 
+
+
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
 
 if __name__ == '__main__':
     app.run()
