77769b847b5fb886fe0924cb729f9c9f21747853
syntaxSizer <kakuma4never@gmail.com>
2016-08-12 14:58:28 +0300

    change application process type

--- manage.py
+++ manage.py
@@ -8,11 +8,13 @@ from tumblood import app
 manager = Manager(app)
 
 # Turn on debugger by default and reloader
-manager.add_command("runserver", Server(
-    use_debugger = True,
-    use_reloader = True,
-    host = '0.0.0.0')
-)
+#to enable python shell withen the context of the app enable this line of code
+# and change th app.run() to manage.run()
+#manager.add_command("runserver", Server(
+use_debugger = True,
+use_reloader = True,
+host = '0.0.0.0'
+#)
 
 if __name__ == "__main__":
-    manager.run()
+    app.run()
